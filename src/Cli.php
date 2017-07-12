900a97a6c402ce37dac0ab1aed315ee697c2c6df
ramazan <yaru@bk.ru>
2017-07-12 15:12:41 +0300

    add game 'even'

--- src/Cli.php
+++ src/Cli.php
@@ -5,9 +5,32 @@ namespace BrainGames\Cli;
 use function \cli\line;
 use function \cli\prompt;
 
+const CORRECT_ANSWERS_TO_WIN = 3;
+
 function run()
 {
-    line('Welcome to the Brain Game!');
+    line("\nWelcome to the Brain Game!");
+    line('Answer "yes" if number even otherwise answer "no".' . PHP_EOL);
     $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    line("Hello, $name!" . PHP_EOL);
+
+    $correct_answers = 0;
+
+    while ($correct_answers < CORRECT_ANSWERS_TO_WIN) {
+        $number = rand(1, 20);
+        line("Question: $number");
+
+        $answer = prompt('Your answer');
+        $expected = $number % 2 === 0 ? 'yes' : 'no';
+
+        if ($answer === $expected) {
+            line("Correct!");
+            $correct_answers++;
+        } else {
+            line("'$answer' is wrong answer ;(. Correct answer was '$expected'.");
+            line("Let's try again, $name!");
+        }
+    }
+
+    line("Congratulations, $name!");
 }
