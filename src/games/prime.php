49f998aa44e94a135465a90e642419c403fb082b
ramazan <yaru@bk.ru>
2017-07-15 13:09:03 +0300

    add game prime

---
+++ src/games/prime.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace BrainGames\game\prime;
+
+use function BrainGames\Cli\startGame;
+
+const INSTRUCTION = 'Answer "yes" if number is prime otherwise answer "no".';
+
+
+function run()
+{
+    $getQuestion = function () {
+        $question = rand(1, 20);
+        return $question;
+    };
+
+
+    $getExpected = function ($question) {
+        $expected =  isPrime($question) ? 'yes' : 'no';
+        return $expected;
+    };
+
+    startGame(INSTRUCTION, $getQuestion, $getExpected);
+}
+
+function isPrime($question)
+{
+    if ((int)$question <= 2) {
+        return true;
+    }
+
+    $result = function ($i) use (&$result, $question) {
+        if ($question % $i == 0) {
+            return false;
+        } elseif ($i >= $question/2) {
+            return true;
+        }
+        return $result($i+1);
+    };
+    return $result(2);
+}
